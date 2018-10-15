0afb91cdc79e09562bb47490e830188e3f58ce91
jelek21 <j.lekieffre@cranfield.co.uk>
2018-10-15 16:49:00 +0100

    Ajout des commentaire (doxygen ready)

--- Exercise1SortingCpp/Output.h
+++ Exercise1SortingCpp/Output.h
@@ -6,6 +6,20 @@ using namespace std;
 
 class Output{
 public:
+
+	/**
+	* Normal public method that returns int (a status).
+	* It prints a vector of any number of integers (the length is computed inside the function).
+	* @parameter vector<int> the vector of integer you want to print.
+	* @return  An status in (0=OK !0=NOK).
+	*/
 	int PrintIntVector(vector<int> V);
+
+	/**
+	* Normal public method that returns int (a status).
+	* It prints a vector of any number of integers (the length is computed inside the function).
+	* @parameter A String S, the string you want to print and a bool Eline (endl at the end of the print or not).
+	* @return  An status in (0=OK !0=NOK).
+	*/
 	int PrintString(string S, bool Eline);
 };
