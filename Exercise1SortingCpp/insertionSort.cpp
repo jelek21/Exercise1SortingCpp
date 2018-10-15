9201394188dda0bdf62337484a859c7694d2c3bc
MeterMatters <Geoffroydubruel@hotmail.fr>
2018-10-15 18:29:21 +0200

    insertionSort

---
+++ Exercise1SortingCpp/insertionSort.cpp
@@ -0,0 +1,31 @@
+#include "insertionSort.h"
+
+
+
+
+//Insertion Sort Algorithm
+std::pair<vector<int>, vector<int>> insertionSort::SortIntVector(vector<int> list)
+{
+	int  j, k;
+	vector<int> aux(list.size());
+
+	for (int i = 1; i < list.size(); i++)
+	{
+		j = i;
+		k = list[i];
+
+		while (j > 0 && k < list[j - 1])
+		{
+			list[j] = list[j - 1];
+			j--;
+		}
+		aux[i] = j;
+		list[j] = k;
+
+
+	}
+
+
+
+	return { list, aux };
+}
---
+++ Exercise1SortingCpp/insertionSort.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class insertionSort {
+public:
+
+
+	std::pair<vector<int>, vector<int>> SortIntVector(vector<int> list);
+
+};
