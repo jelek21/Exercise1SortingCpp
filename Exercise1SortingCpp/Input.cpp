6d19b8b68670ce22513ffdfc55c12179c00d0c3e
jelek21 <j.lekieffre@cranfield.co.uk>
2018-10-15 16:28:34 +0100

    Created Input and Output classes functions

--- Exercise1SortingCpp/Input.cpp
+++ Exercise1SortingCpp/Input.cpp
@@ -1,1 +1,17 @@
 #include "Input.h"
+
+std::vector<int> Input::AskIntVector()
+{
+	int length;
+	int Integer;
+	cout << "Please enter the length of the array you want to create" << endl;
+	cin >> length;
+	vector<int> returnTable(length);
+	cout << "Please enter the " << length << "Integers" << endl;
+	for (int i = 0; i < length; i++) {
+		cout << "Integer " << i+1 << " of " << length;
+		cin >> Integer;
+		returnTable[i] = Integer;
+	}
+	return returnTable;
+}
--- Exercise1SortingCpp/Output.cpp
+++ Exercise1SortingCpp/Output.cpp
@@ -1,3 +1,18 @@
 #include "Output.h"
 
 
+int Output::PrintIntVector(vector<int> V)
+{
+	for (int i = 0; i < V.size(); i++) {
+		cout << V[i] << "    ";
+	}
+	cout << endl;
+	return 0
+}
+
+int Output::PrintString(string S, bool Eline)
+{
+	cout << S;
+	if (Eline) cout << endl;
+	return 0;
+}
