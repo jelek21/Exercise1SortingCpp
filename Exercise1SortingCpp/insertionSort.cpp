55b423b8e92472ada5d8b777a5ac71795bb7d8a3
MeterMatters <Geoffroydubruel@hotmail.fr>
2018-10-16 16:06:39 +0200

    Indexes vector bug resolved

--- Exercise1SortingCpp/insertionSort.cpp
+++ Exercise1SortingCpp/insertionSort.cpp
@@ -1,24 +1,31 @@
 #include "insertionSort.h"
+#include "Output.h"
 
 /* Insertion Sort Algorithm  */
 std::pair<vector<int>, vector<int>> insertionSort::SortIntVector(vector<int> list)
 {
 	int  j, k;
 	int listsize = list.size();
-	vector<int> aux(listsize);
+	vector<int> index(listsize);
+	index[0] = 0;
 	
 	for (int i = 1; i < listsize; i++)
 	{
+
 		j = i;
 		k = list[i];
+		index[i] = i;
 
 		while (j > 0 && k < list[j - 1])
 		{
 			list[j] = list[j - 1];
+			index[j] = j - 1;
 			j--;
+
 		}
-		aux[i] = j;
+		index[j] = i;
+
 		list[j] = k;
 	}
-	return { list, aux };
+	return { list, index };
 }
