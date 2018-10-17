9536ce4f108982772d91e8de2bfb3a8242b95878
MeterMatters <Geoffroydubruel@hotmail.fr>
2018-10-17 13:05:08 +0200

    insertionSort with pair

--- Exercise1SortingCpp/insertionSort.cpp
+++ Exercise1SortingCpp/insertionSort.cpp
@@ -1,31 +1,64 @@
 #include "insertionSort.h"
 #include "Output.h"
 
-/* Insertion Sort Algorithm  */
+// Insertion Sort class 
 std::pair<vector<int>, vector<int>> insertionSort::SortIntVector(vector<int> list)
 {
-	int  j, k;
+	int  j;
+	pair<int, int> k;
 	int listsize = list.size();
-	vector<int> index(listsize);
-	index[0] = 0;
-	
+
+	//Creation of vector of pair used for keeping the indexes
+	vector<pair<int,int>> alt(listsize);
+
+	for (int i = 0; i < listsize; i++)
+	{
+
+		alt[i].first = list[i];
+		alt[i].second = i;
+
+	}
+
+	//Insertion sorting the vector 
 	for (int i = 1; i < listsize; i++)
 	{
 
 		j = i;
-		k = list[i];
-		index[i] = i;
+		k = alt[i];
 
-		while (j > 0 && k < list[j - 1])
+
+		while (j > 0 && k < alt[j - 1])
 		{
-			list[j] = list[j - 1];
-			index[j] = j - 1;
+
+			alt[j] = alt[j - 1];
+
 			j--;
 
 		}
 
-		index[j] = i;
-		list[j] = k;
+		alt[j] = k;
 	}
+
+	//Creation of index vector
+	vector<int> index(listsize);
+
+	for (int i = 0; i < listsize; i++)
+	{
+
+		index[i] = alt[i].second;
+
+	}
+
+	//Creation of list vector
+
+	for (int i = 0; i < listsize; i++)
+	{
+
+		list[i] = alt[i].first;
+
+	}
+
+
+	//return of the two vectors 
 	return { list, index };
 }
