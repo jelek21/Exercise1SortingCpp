985dcab524161e3b2c0b7c77e25bcebc550c1249
jelek21 <J.Lekieffre@cranfield.ac.uk>
2018-10-17 12:16:04 +0100

    Fix quicksort issue plus phrases in main.cpp

--- Exercise1SortingCpp/main.cpp
+++ Exercise1SortingCpp/main.cpp
@@ -14,19 +14,18 @@ int main(){
 	V = Input::AskIntVector();
 	vector<int> sorted = get<0>(insertionSort::SortIntVector(V));
 	vector<int> indexes = get<1>(insertionSort::SortIntVector(V));
-	a = Output::PrintString("The unsorted vector is: ", true);
+	a = Output::PrintString("The vector you entered is: ", true);
 	a = Output::PrintIntVector(V);
-	a = Output::PrintString("The sorted vector is: ", true);
+	a = Output::PrintString("The vector sorted by the insdertion algorithm is: ", true);
 	a = Output::PrintIntVector(sorted);
 	a = Output::PrintString("The indexes vector is: ", true);
 	a = Output::PrintIntVector(indexes);
 	a = Output::PrintString("", true);
 	QuickSort QSort;
 	vector<int> W = QSort.StartSorting(V);
-	a = Output::PrintString("The sorted vector is: ", true);
-	a = Output::PrintIntVector(V);
+	a = Output::PrintString("The vector sorted by the Quicksort algorithm is : ", true);
+	a = Output::PrintIntVector(W);
 	
 	a = Output::PrintString("Please type any key to end", true);
 	cin >> a;
 }
-
